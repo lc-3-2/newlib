bc05111790a44a2a4ea289414c2e8086bc5a895d
Ammar Ratnani <ammrat13@gmail.com>
2023-09-01 17:58:32 -0500

    Add documentation to `kill`

--- libgloss/lc32/x/_kill.c
+++ libgloss/lc32/x/_kill.c
@@ -10,13 +10,17 @@ int _kill(int pid, int sig) {
 
   // Check whether we're trying to send a signal to a process that doesn't
   // exist. We only have the init process.
-  if (!(pid == 1 || pid == 0 || pid == -1)) {
+  if (!(pid == 1 || pid == 0)) {
     errno = ESRCH;
     return -1;
   }
 
   // We tried to send a signal to PID 1. But, init only handles signals that
   // have been explicitly registered. Trying to send any other signal fails.
+  //
+  // FIXME: We don't actually check if we registered a signal handler for the
+  // incoming signal. Therefore, this code only works when called from `raise`,
+  // which does the checking for us. For now, use `raise` instead of `kill`.
   errno = EPERM;
   return -1;
 }
