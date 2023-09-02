be373ae43aaf964766bf699a3ab9b0e456a3060d
Ammar Ratnani <ammrat13@gmail.com>
2023-09-02 18:10:48 -0500

    Add `Rngmmio` capability

---
+++ libgloss/lc32/Rngmmio/_getentropy.c
@@ -0,0 +1,27 @@
+#include <_syslist.h>
+#include <errno.h>
+#include <stdint.h>
+
+// Remember, all system-level code uses the global variable errno
+// See: newlib/libc/reent/reent.c
+#undef errno
+extern int errno;
+
+// Address of the RNG register. Set by the linker script.
+volatile uint32_t __lc32_mmio_rng;
+
+int _getentropy(void *buffer, size_t length) {
+
+  // Convenience variables - copies of the parameters we modify
+  char *buf = buffer;
+  size_t to_go = length;
+
+  // Copy one byte at a time. This way, we don't have to worry about alignment
+  //
+  // FIXME: Performance
+  // See: newlib/libc/string/memset.c
+  while (to_go--)
+    *buf++ = (char)__lc32_mmio_rng;
+
+  return 0;
+}
